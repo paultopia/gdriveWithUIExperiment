9d825b3513d07ff7fd803b9530a30bc7ea1c29fc
Paul Gowder <paul.gowder@gmail.com>
2019-05-15 11:02:27 -0400

    Initial Commit

---
+++ gdriveWithUIExperiment/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gdriveWithUIExperiment
+//
+//  Created by Paul Gowder on 5/15/19.
+//  Copyright © 2019 Paul Gowder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
