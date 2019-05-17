4fe143c8ce5f976093e2f9653c8ba6ada4a9316b
Paul Gowder <paul.gowder@gmail.com>
2019-05-17 15:30:02 -0400

    more incremental work on the multipart upload

--- gdriveWithUIExperiment/uploadFile.swift
+++ gdriveWithUIExperiment/uploadFile.swift
@@ -21,23 +21,72 @@ extension URLRequest {
     }
 }
 
+//protocol Extendable {}
+//extension String: Extendable {}
+
+extension Dictionary {
+    func and(_ otherDicts: [Dictionary]) -> Dictionary{
+        return otherDicts.reduce(self, { x, y in
+            x.merging(y) { (current, _) in current }
+        })
+    }
+}
+
 // IN PROGRESS
 
-struct multipartHTTPUpload {
-    var params: [String: String]
-    
-    func makeBoundary() -> String {
-        // write me
+struct multipartUploadPart {
+    var headers = ["Content-Disposition": "form-data"]
+    init(metadata: [String: String]) {
         
-        return " "
     }
+    init(media: URL){
+        
+    }
+}
+
+
+struct multipartRelatedUpload {
+    var headers: [String: String]
+    var params: [String: String]
+    var boundary: String
+    var metadataPart: multipartUploadPart
+    var mediaPart: multipartUploadPart
+    
+    init(metadata: [String: String],
+         media: URL,
+         extraParameters: [String: String]? = nil,
+         extraHeaders: [String: String]? = nil) {
+        let requiredParameter = ["uploadType":"multipart"]
+        if let params = extraParameters {
+            self.params = requiredParameter.and([params])
+        }
+        else {
+            self.params = requiredParameter
+        }
+        
+        let bdry = "--\(UUID().uuidString)--"
+        boundary = bdry
+        let requiredHeader = ["Content-Type": "multipart/related; boundary=\(bdry)"]
+        
+        if let hdrs = extraHeaders {
+            self.headers = requiredHeader.and([hdrs])
+        }
+        else {
+            self.headers = requiredHeader
+        }
+        
+        metadataPart = multipartUploadPart(metadata: metadata)
+        mediaPart = multipartUploadPart(media: media)
+        
+    }
+    
     
     func authHeader() -> [String: String]? {
         guard let token = accessToken.get() else {
             print("no access token")
             return nil
         }
-        return ["Authorization": "Bearer " + token]
+        return ["Authorization": "Bearer \(token)"]
     }
 
 }
