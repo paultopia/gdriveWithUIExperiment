6b6c928c04182c8c1d3f06b9f0ff45f91e8f610e
Paul Gowder <paul.gowder@gmail.com>
2019-05-30 14:41:10 -0500

    fix a couple more compiler errors

--- gdriveWithUIExperiment/debuggingCode.swift
+++ gdriveWithUIExperiment/debuggingCode.swift
@@ -12,17 +12,6 @@ func basicLocalhostCall() {
     fetch(url: "http://localhost:8888/", queries: ["Foo": "Bar"], callback: {print($0)})
 }
 
-func testUploadFormat() {
-    let testRequest = MultipartRelatedUpload(testString: "this is a test string body").composeRequest()
-    print("\n\nHEADERS: \n\n")
-    print(testRequest.allHTTPHeaderFields!)
-    print("\n\nBODY: \n\n")
-    print(String(decoding: testRequest.httpBody!, as: UTF8.self))
-    print("\n\nURL: \n\n")
-    print(testRequest.url!)
-    print("\n\nmethod: \n\n")
-    print(testRequest.httpMethod!)
-}
 
 func testInLocalEchoServer(){
     let dialog = NSOpenPanel()
@@ -38,7 +27,7 @@ func testInLocalEchoServer(){
         
         let result = dialog.url!
         // NOW UPLOAD IT HERE.
-        let request = MultipartRelatedUpload(result)
+        var request = MultipartRelatedUpload(result)
         request.post(callback: {print(String(data: $0, encoding: .utf8)!)}, testing: true)
         
     } else {
